4a4388c541e92f51442ed93d1db432eb27f720b8
CloudCluster <nikarampo@windowslive.com>
2023-07-26 21:20:50 +0300

    Add form validation

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ function hideMessage() {
     document.getElementById("form").style.display = 'flex';
 }
 
-function showMessage() {
+function validateForm() {
+   const email = document.getElementById("email");
+   if (email.validity.valid) {
     document.getElementById("form").style.display = 'none';
     document.getElementById("success").style.display = 'flex';
+   } else {
+    document.getElementById("error").style.display = 'block';
+    document.getElementById("email").style.borderColor =  'hsl(4, 100%, 67%)';
+    document.getElementById("email").style.backgroundColor =  'hsla(4, 100%, 67%, 20%)';
+    document.getElementById("email").style.color =  'hsl(4, 100%, 67%)';
+   }
 }
